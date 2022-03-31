169c1361b3ab843d1d620a27bb6225bbdf9aad5b
MonikaKeller <monika.keller.szne@gmail.com>
2022-03-31 14:58:33 +0200

    y

--- Locations/locations_test.py
+++ Locations/locations_test.py
@@ -6,6 +6,10 @@ from selenium.webdriver.common.by import By
 
 from webdriver_manager.chrome import ChromeDriverManager
 
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 import requests
 
 @pytest.fixture
@@ -19,13 +23,25 @@ def driver():
 def test_create(driver:webdriver.Chrome):
     requests.delete ("http://127.0.0.1:8080/api/locations")
     driver.find_element(By.LINK_TEXT,"Create location").click()
-    driver.find_element(By.CLASS_NAME,"btn-primary").click()
+    
     
     driver.find_element(By.ID,"location-name").send_keys("Home")
     driver.find_element(By.ID,"location-coords").send_keys("1,1")
     
-    
+    driver.find_element(By.CLASS_NAME,"btn-primary").click()
     #Then
-    #message = driver.find_element(By.ID,"message div").text
-    #assert message == 'Location has been created'
+   
+    wait = WebDriverWait (driver, 3)
+    wait.until(EC.text_to_be_present_in_element((By.ID,"message-div"), "Location has been created"))
+    
+    message = driver.find_element(By.ID,"message-div").text
+    assert message == 'Location has been created'
+    
+    name_cell_text = driver.find_element(By.CSS_SELECTOR, "#locations-table-tbody > tr:nth-child(1) > td:nth-child(2)").text
+    
+    assert name_cell_text == 'Home'
+    
+    name_cell_coordinate = driver.find_element(By.CSS_SELECTOR, "#locations-table-tbody > tr > td:nth-child(3))").text
+    
+    assert name_cell_coordinate == '1, 1'
     pass
